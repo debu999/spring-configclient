8fa7369c11b91d40e85aeda43592f96b1084a553
Debabrata Patnaik <debabrata_patnaik@live.com>
2024-03-26 08:59:39 +0530

    centralize properties

--- src/main/java/org/doogle/configclient/DynamicConfigurations.java
+++ src/main/java/org/doogle/configclient/DynamicConfigurations.java
@@ -1,11 +1,13 @@
 package org.doogle.configclient;
 
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @Component
 @RefreshScope
+@Data
 public class DynamicConfigurations {
 
   @Value("${rest.application.environment:#{null}}")
--- src/main/java/org/doogle/configclient/ValueController.java
+++ src/main/java/org/doogle/configclient/ValueController.java
@@ -3,23 +3,25 @@ package org.doogle.configclient;
 
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/data")
-@RefreshScope
 public class ValueController {
 
+  private final DynamicConfigurations dynamicConfigurations;
   @Value("${rest.application.environment:#{null}}")
   public String environment;
 
+  public ValueController(DynamicConfigurations dynamicConfigurations) {
+    this.dynamicConfigurations = dynamicConfigurations;
+  }
 
   @GetMapping("/")
   public Map<String, String> getProperties() {
-    return Map.of("environment", environment);
+    return Map.of("environment", dynamicConfigurations.getEnvironment());
   }
 
 }
