c927eb7c389f98ccb29178511e4cfb4712442ed7
Debabrata Patnaik <debabrata_patnaik@live.com>
2024-03-26 08:36:36 +0530

    rename main application..

--- src/main/java/org/doogle/configclient/ConfigClientApplication.java
+++ src/main/java/org/doogle/configclient/ConfigClientApplication.java
@@ -6,11 +6,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class SpringConfigClientApplication {
+public class ConfigClientApplication {
 
 
   public static void main(String[] args) {
-    SpringApplication.run(SpringConfigClientApplication.class, args);
+    SpringApplication.run(ConfigClientApplication.class, args);
   }
 
 }
